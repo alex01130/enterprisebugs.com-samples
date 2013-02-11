1bb205a4ed12ae499b5da9d590fb34c4191e1672
enterprisebug <daniel.sirz@gmail.com>
2013-02-11 15:30:56 +0100

    Consume a WebService Sample

---
+++ ConsumeWebservice/WebServiceClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using EnterpriseBugs.WebServiceDemo.WebServiceClient.com.ezzylearning.www;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterpriseBugs.WebServiceDemo.WebServiceClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // get a instance of the webservice
+            CountryInformationService service = new CountryInformationService();
+            // call the method
+            string result = service.GetPopulationByCountry("Germany");
+
+            Console.WriteLine("Germany has a population of: " + result);
+            Console.ReadLine();
+
+        }
+    }
+}
