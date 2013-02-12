cd4999df29ef000d0264aabb9c1a82819e3f5de1
enterprisebug <daniel.sirz@gmail.com>
2013-02-12 16:38:08 +0100

    Added Typemock DateTime Sample

---
+++ ControllingWithTypemock/BusinessLogic.Test/DateTimeHelperTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TypeMock.ArrangeActAssert;
+
+namespace EnterpriseBugs.ControllingWithTypemock.BusinessLogic.Test
+{
+    [TestClass, Isolated(DesignMode.Pragmatic)]
+    public class DateTimeHelperTest
+    {
+        private TestContext testContextInstance;
+        public TestContext TestContext
+        {
+            get { return testContextInstance; }
+            set { testContextInstance = value; }
+        }
+
+        /// <summary>
+        ///A test for GetImageForCurrentTime
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("BusinessLogic.Test\\TestData\\GetImageForCurrentTime_TestBoundaries.xml")]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", "|DataDirectory|\\GetImageForCurrentTime_TestBoundaries.xml", "Row", DataAccessMethod.Sequential)]
+        public void GetTextForCurrentDateTest()
+        {
+            int hourToTest = Int32.Parse((string)TestContext.DataRow["HourToTest"]);
+            string assertValue = (string)TestContext.DataRow["AssertValue"];
+
+            Execute_GetTextForCurrentDateTest(hourToTest, assertValue);
+        }
+
+        private void Execute_GetTextForCurrentDateTest(int hourToTest, string assertValue)
+        {
+            #region Arrange
+
+            string expected = assertValue;
+            // If DateTime.Now is called, our DateTime wil be returned
+            DateTime controlledDate = new DateTime(2013, 1, 1, hourToTest, 0, 0);
+            // This is very the Typemock magic happens!
+            Isolate.WhenCalled(() => DateTime.Now).WillReturn(controlledDate);
+
+            #endregion
+
+            #region Act
+
+            string actual = DateTimeHelper.GetTextForCurrentDate();
+
+            #endregion
+
+            #region Assert
+
+            Assert.AreEqual(expected, actual);
+
+            #endregion
+        }
+    }
+}
---
+++ ControllingWithTypemock/BusinessLogic/DateTimeHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterpriseBugs.ControllingWithTypemock.BusinessLogic
+{
+    public static class DateTimeHelper
+    {
+        /// <summary>
+        /// Returns an text according to the current time
+        /// </summary>
+        /// <returns></returns>
+        /// <remarks>
+        /// Uhrzeiten die unterschieden werden sollen:
+        /// <list type="bullet">
+        /// <item>23-6: night</item>
+        /// <item>7-10: morning</item>
+        /// <item>11-13: noon</item>
+        /// <item>14-22: evening</item>
+        /// </list>
+        /// </remarks>
+        public static string GetTextForCurrentDate()
+        {
+            DateTime currentTime = DateTime.Now;
+
+            if (currentTime.Hour >= 23 || currentTime.Hour <= 6)
+            {
+                return "night";
+            }
+            if (currentTime.Hour >= 7 && currentTime.Hour <= 10)
+            {
+                return "morning";
+            }
+            if (currentTime.Hour >= 11 && currentTime.Hour <= 13)
+            {
+                return "noon";
+            }
+            if (currentTime.Hour >= 14 && currentTime.Hour <= 22)
+            {
+                return "evening";
+            }
+
+            throw new NotImplementedException("This sould never happen");
+        }
+    }
+}
