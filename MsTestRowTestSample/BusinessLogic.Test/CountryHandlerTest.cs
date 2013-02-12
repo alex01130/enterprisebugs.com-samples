f04ba7b76edbb52c140a359571ff8fd47f48db9e
enterprisebug <daniel.sirz@gmail.com>
2013-02-12 13:57:44 +0100

    Sample DataDriven RowTest MSTest

---
+++ MsTestRowTestSample/BusinessLogic.Test/CountryHandlerTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EnterpriseBugs.MsTestRowTestSample.BusinessLogic.Test
+{
+    [TestClass]
+    public class CountryHandlerTest
+    {
+        private TestContext testContextInstance;
+        public TestContext TestContext
+        {
+            get { return testContextInstance; }
+            set { testContextInstance = value; }
+        }
+
+
+        [TestMethod]
+        [DeploymentItem("BusinessLogic.Test\\TestData\\GetImageForCountry.xml")]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", "|DataDirectory|\\GetImageForCountry.xml", "Row", DataAccessMethod.Sequential)]
+        public void GetImageForCountry_SeveralTests()
+        {
+            // Get data out of the XML File
+            string country = testContextInstance.DataRow["Country"].ToString();
+            string image = testContextInstance.DataRow["Image"].ToString();
+
+            Execute_GetImageForCountry_SeveralTests(country, image);
+        }
+
+        /// <summary>
+        /// Helper Method that executes the test
+        /// </summary>
+        /// <param name="country"></param>
+        /// <param name="image"></param>
+        private void Execute_GetImageForCountry_SeveralTests(string country, string image)
+        {
+            #region Arrange
+
+            string expected = image;
+            CountryHandler target = new CountryHandler();
+
+            #endregion
+
+            #region Act
+
+            string actual = target.GetImageForCountry(country);
+
+            #endregion
+
+            #region Assert
+
+            Assert.AreEqual(expected, actual);
+
+            #endregion
+        }
+    }
+}
---
+++ MsTestRowTestSample/BusinessLogic/CountryHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterpriseBugs.MsTestRowTestSample.BusinessLogic
+{
+    public class CountryHandler
+    {
+        /// <summary>
+        /// Get the Image for a given country
+        /// </summary>
+        /// <remarks>
+        /// The following requirements must be fulfilled:
+        /// <list type="bullet">
+        /// <item>"germany" return "germany.gif"</item>
+        /// <item>"usa" return "usa.gif"</item>
+        /// <item>all other countries return "unknown.gif"</item>
+        /// </list>
+        /// </remarks>
+        /// <param name="country"></param>
+        /// <returns></returns>
+        public string GetImageForCountry(string country)
+        {
+            if (country == "germany")
+            {
+                return "germany.gif";
+            }
+            if (country == "usa")
+            {
+                return "usa.gif";
+            }
+
+            return "unknown.gif";
+        }
+    }
+}
